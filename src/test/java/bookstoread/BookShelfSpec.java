25c6ea86d1f4d401f843f1953ead95e6b46a0d03
saurabh <saurabh.kumar7064@gmail.com>
2022-08-07 13:40:20 -0500

    some null asserts

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -1,18 +1,31 @@
 package bookstoread;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("A bookshelf")
 public class BookShelfSpec {
     @Test
+    @DisplayName("is empty when no book is added to it")
     void shelfEmptyWhenNoBookAdded(TestInfo testInfo) {
         System.out.println("Working on test case " + testInfo.getDisplayName());
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
+
+    @Test
+    void nullAssertionTest() {
+        String str = null;
+        assertNull(str);
+        assertNull(str, "str should be null");
+        assertNull(str, () -> "str should be null");
     }
 
 
